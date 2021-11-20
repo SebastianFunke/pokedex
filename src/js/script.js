d8e673ab6e1d3ed348eafbb1ca9aee5e0335c044
unknown <funke.sebastian@web.de>
2021-11-20 16:18:51 +0100

    added search

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,18 @@ let pname;
 let types = [];
 let colors = [];
 let offset = 0;
-let limit = 100;
+let limit = 30;
 let adress;
+let id;
+let centerColor = 'grey'
+let responseGroup = [];
+let searchedPokemons = [];
 
 function init() {
+    loadAllPokemons();
+
     loadPokeGroup();
+    console.log(responseGroup);
 }
 
 async function loadPokeGroup() {
@@ -16,7 +23,6 @@ async function loadPokeGroup() {
     offset += 20;
     let response = await fetch(url);
     responseGroup = await response.json();
-    console.log(currentPokemon);
     for (let i = 0; i < responseGroup['results'].length; i++) {
         loadPokemon(responseGroup['results'][i]['name']);
 
@@ -25,53 +31,83 @@ async function loadPokeGroup() {
 }
 
 
+async function loadAllPokemons() {
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=2000`;
+    let response = await fetch(url);
+    allPokemons = await response.json();
+    console.log(allPokemons);
+
+}
+
+function searchPoke(val) {
+    searchedPokemons = [];
+    for (var i = 0; i < allPokemons['results'].length; i++) {
+        if (allPokemons['results'][i]['name'].includes(val)) {
+            console.log(allPokemons['results'][i]['name']);
+            searchedPokemons.push(allPokemons['results'][i]['name']);
+
+        }
+        console.log(searchedPokemons);
+    }
+    document.getElementById('pokeList').innerHTML = '';
+    for (let i = 0; i < searchedPokemons.length; i++) {
+        if (i > 31) { break; }
+        loadPokemon(searchedPokemons[i]);
+    }
+
+}
+
 async function loadPokemon(name) {
     let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
     let response = await fetch(url);
     currentPokemon = await response.json();
-    console.log(currentPokemon);
-
     pname = currentPokemon['name'];
+    console.log(currentPokemon);
     types = [];
     colors = [];
     for (let i = 0; i < currentPokemon['types'].length; i++) {
         types.push(currentPokemon['types'][i]['type']['name']);
         colors.push(currentPokemon['types'][i]['type']['name']);
     }
+    if (colors.length < 2) {
+        colors.push(colors[0]);
+        centerColor = colors[0];
+    } else {
+        centerColor = 'grey';
+    }
     let adress = currentPokemon['sprites']['front_default'];
-    console.log(pname);
-    console.log(types);
-    console.log(colors);
-    console.log(getSnippet(pname, adress));
-
+    id = currentPokemon['id'];
     document.getElementById('pokeList').innerHTML += getSnippet(pname, adress);
 }
 
 
 function getColorSnippet() {
     let returnString = '';
-    console.log('return: ' + returnString);
-    for (let i = 0; i < colors.length; i++) {
+    //<p class="pokeType1" style="background-color: var(--${colors[i]});color: white;">${types[i]}</p>`
+    for (let i = 0; i < types.length; i++) {
         returnString += `<p class="pokeType1" style="background-color: var(--${colors[i]});color: white;">${types[i]}</p>`
     }
-    console.log('return: ' + returnString);
     return returnString;
+}
+
+function loadPokedetails(pokename) {
+    console.log(pokename);
 }
 
 function getSnippet(pokename, spriteAdress) {
     return `<div class="mb-3 col-xl-3 col-lg-4 col-md-6">
-    <div class="pokecontainer" style="background-color: var(--${colors[0]});">
-        <div class="m-1 ">
+    <div class="pokecontainer" onclick="loadPokedetails('${pokename}')" style="background: linear-gradient(140deg, var(--${colors[0]}) 0%, var(--${colors[0]}) 40%,var(--${centerColor}) 50%,var(--${colors[1]}) 60%,var(--${colors[1]}) 100%);">
+    <div class="m-1 pokeHead">
+            <h3>#${id}<h3>
             <h2>${pokename}</h2>
         </div>
         <div class="pokeContainerLowerBox row ">
             <div class="m-0 p-0 pokeTypes col-4 ">` +
         getColorSnippet() +
         `
-                
             </div>
             <div class="m-0 p-0 col-8 ">
-                <img class="pokePic " src="${spriteAdress}" alt=" ">
+                <img class="pokePic" src="${spriteAdress}" alt=" ">
             </div>
         </div>
     </div>
