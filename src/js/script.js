a04ec5f11e0970d1d019464c7e777682132b8729
unknown <funke.sebastian@web.de>
2021-12-03 23:11:07 +0100

    added comments

--- src/js/script.js
+++ src/js/script.js
@@ -12,8 +12,13 @@ let searchedPokemons = [];
 let localPoke = 'detailPoke';
 let searchedPokeLocal = 'searched';
 let searchPos = 0;
+let val;
+let inProgress = false;
+
 /**
- * function loaded at first
+ * function loaded at first by index.html
+ * If search parameters are saved locally, they will be loaded and the pokemons you are looking for will be displayed.
+ * If not, a random group will be loaded
  */
 async function init() {
     await loadAllPokemons();
@@ -23,15 +28,14 @@ async function init() {
     } else {
         loadPokeGroup();
     }
-
 }
 
+/**
+ * function to load a group of pokemons.
+ * if there are parameters in the search bar, a group of the pokemons you are looking for is loaded.
+ * if not, a group of random pokemons will be loaded
+ */
 async function loadPokeGroup() {
-    /*
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    offset += 20;
-    let response = await fetch(url);
-    responseGroup = await response.json();*/
     if (document.getElementById('inputSearch').value == '') {
         for (let i = 0; i < limit; i++) {
             loadPokemon(allPokemons['results'][offset + i]['name']);
@@ -39,44 +43,69 @@ async function loadPokeGroup() {
         offset += limit;
     } else {
         for (let i = 2; i < searchedPokemons.length - searchPos; i++) {
-            console.log(searchPos);
-            console.log(searchedPokemons.length - searchPos);
-            if (i > 19) { break; }
+            if (i > limit) { break; }
             loadPokemon(searchedPokemons[searchPos]);
             searchPos++;
         }
+
     }
 }
 
+
+/**
+ * loads all names of existing pokemons into the json
+ */
 async function loadAllPokemons() {
     let url = `https://pokeapi.co/api/v2/pokemon?limit=2000`;
     let response = await fetch(url);
     allPokemons = await response.json();
 }
 
+/**
+ * function to search for specific pokemon
+ */
 async function searchPoke() {
     await new Promise(resolve => setTimeout(resolve, 100));
-    let val = document.getElementById('inputSearch').value;
-    console.log(val);
-    searchPos = 0;
-    offset = limit;
-    saveArrayToLocalStorage(searchedPokeLocal, val)
-    document.getElementById('pokeList').innerHTML = '';
-    searchedPokemons = [];
+    document.getElementById('loadMoreContainer').classList.remove('d-none');
+    clearElements();
     for (var i = 0; i < allPokemons['results'].length; i++) {
         if (allPokemons['results'][i]['name'].includes(val)) {
             searchedPokemons.push(allPokemons['results'][i]['name']);
         }
     }
-
     for (let i = 0; i < searchedPokemons.length; i++) {
-        if (i > 19) { break; }
+        if (i > limit) { break; }
         loadPokemon(searchedPokemons[searchPos]);
         searchPos++;
     }
-
+    checkMoreBtnHide();
 }
 
+/**
+ * if there are no further entries in the search results, the button will be hidden
+ */
+function checkMoreBtnHide() {
+    if (searchedPokemons.length - searchPos < 2) {
+        document.getElementById('loadMoreContainer').classList.add('d-none');
+    }
+}
+
+/**
+ * clear and reset variables for the searchPoke function
+ */
+function clearElements() {
+    val = document.getElementById('inputSearch').value;
+    searchPos = 0;
+    offset = limit;
+    saveArrayToLocalStorage(searchedPokeLocal, val)
+    document.getElementById('pokeList').innerHTML = '';
+    searchedPokemons = [];
+}
+
+/**
+ * load and display a single pokemon
+ * @param {String} name name of a pokemon
+ */
 async function loadPokemon(name) {
     let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
     let response = await fetch(url);
@@ -84,6 +113,15 @@ async function loadPokemon(name) {
     pname = currentPokemon['name'];
     types = [];
     colors = [];
+    setColorAndTypes();
+    adress = getSprite();
+    document.getElementById('pokeList').innerHTML += getSnippet(pname, adress);
+}
+
+/**
+ * match the colors to the types of pokemon
+ */
+function setColorAndTypes() {
     for (let i = 0; i < currentPokemon['types'].length; i++) {
         types.push(currentPokemon['types'][i]['type']['name']);
         colors.push(currentPokemon['types'][i]['type']['name']);
@@ -94,11 +132,12 @@ async function loadPokemon(name) {
     } else {
         centerColor = 'grey';
     }
-    adress = getSprite();
-    document.getElementById('pokeList').innerHTML += getSnippet(pname, adress);
 }
 
-
+/**
+ * returns a html code for each type of pokemon in the appropriate colors to display in the document
+ * @returns String
+ */
 function getColorSnippet() {
     let returnString = '';
     //<p class="pokeType1" style="background-color: var(--${colors[i]});color: white;">${types[i]}</p>`
@@ -108,6 +147,10 @@ function getColorSnippet() {
     return returnString;
 }
 
+/**
+ * return of an address of the high resolution image. if none is available, a low-resolution image return will be made
+ * @returns String - adress of the sprite
+ */
 function getSprite() {
     if (currentPokemon['sprites']['other']['home']['front_default']) {
         return adress = currentPokemon['sprites']['other']['home']['front_default'];
@@ -116,61 +159,12 @@ function getSprite() {
     }
 }
 
-
-async function loadPokedetails() {
-    pokename = loadArrayFromLocalStorage(localPoke);
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokename}`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    let progressBars = document.getElementsByClassName('line');
-    for (let i = 0; i < progressBars.length; i++) {
-        progressBars[i].innerHTML = getBar(currentPokemon['stats'][i]['stat']['name'], currentPokemon['stats'][i]['base_stat'])
-    }
-    document.getElementById('detailImg').src = getSprite();
-    document.getElementById('pokeName').innerText = currentPokemon['name'];
-    displayData(pokename);
-    setTitle(currentPokemon['name']);
-}
-
-function getBar(text, val) {
-    let valPercent = val * 0.5;
-    return `
-    <p class="col-3">${text}</p>
-    <div class="progress col-8">
-        <div class="progress-bar" role="progressbar" style="width: ${valPercent}%; background-color: var(--${text});" aria-valuenow="25" aria-valuemin="0" aria-valuemax="200">${val}</div>
-    </div>
-    `
-}
-
-async function displayData(pokename) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokename}`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    for (let i = 0; i < currentPokemon['abilities'].length; i++) {
-        document.getElementById('detailData').innerHTML += getDataAbility(currentPokemon['abilities'][i]['ability']['name']);
-    }
-    if (currentPokemon['types'].length > 1) {
-        document.getElementById('detailTypes').innerHTML += `<h3>Typen:</h3>`
-    } else {
-        document.getElementById('detailTypes').innerHTML += `<h2>Typ:</h2>`
-    }
-    for (let i = 0; i < currentPokemon['types'].length; i++) {
-        document.getElementById('detailTypes').innerHTML += getDataAbility(currentPokemon['types'][i]['type']['name']);
-    }
-}
-
-function getDataAbility(ability) {
-    return `
-    <p class="abilityParagraph">${ability}</p>    
-    `
-}
-
-function getDataTypes(type) {
-    return `
-    <p class="abilityParagraph">${type}</p>    
-    `
-}
-
+/**
+ * 
+ * @param {String} pokename - name of the pokemon
+ * @param {String} spriteAdress - internetadress of the sprite
+ * @returns String - html code to display a container with the specific name of the pokemon
+ */
 function getSnippet(pokename, spriteAdress) {
     return `<div class="mb-3 col-xl-3 col-lg-4 col-md-6">
     <div class="pokecontainer" onclick="openDetailSite('${pokename}')" style="background: linear-gradient(140deg, var(--${colors[0]}) 0%, var(--${colors[0]}) 40%,var(--${centerColor}) 50%,var(--${colors[1]}) 60%,var(--${colors[1]}) 100%);">
@@ -190,27 +184,100 @@ function getSnippet(pokename, spriteAdress) {
 </div>`;
 }
 
+/**
+ * function to save the clicked pokemon name on local storage and 
+ * open the detail site
+ * @param {String} inputPoke - name of the pokemon
+ */
+function openDetailSite(inputPoke) {
+    saveArrayToLocalStorage(localPoke, inputPoke);
+    window.location.href = 'details.html';
+}
 
+/**
+ * Display of the details of a pokemon on details.html
+ */
+async function loadPokedetails() {
+    pokename = loadArrayFromLocalStorage(localPoke);
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokename}`;
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    let progressBars = document.getElementsByClassName('line');
+    for (let i = 0; i < progressBars.length; i++) {
+        progressBars[i].innerHTML = getBar(currentPokemon['stats'][i]['stat']['name'], currentPokemon['stats'][i]['base_stat'])
+    }
+    document.getElementById('detailImg').src = getSprite();
+    document.getElementById('pokeName').innerText = currentPokemon['name'];
+    displayData(pokename);
+    setTitle(currentPokemon['name']);
+}
 
+/**
+ * generate the code to display an ability-bar
+ * @param {String} text - name from the ability
+ * @param {number} val - value from the ability
+ * @returns String - html code for display the bar
+ */
+function getBar(text, val) {
+    let valPercent = val * 0.5;
+    return `
+    <p class="col-3">${text}</p>
+    <div class="progress col-8">
+        <div class="progress-bar" role="progressbar" style="width: ${valPercent}%; background-color: var(--${text});" aria-valuenow="25" aria-valuemin="0" aria-valuemax="200">${val}</div>
+    </div>
+    `
+}
 
-function detailBg() {
+/**
+ * display the types of the pokemon
+ * @param {String} pokename - name of the pokemon
+ */
+async function displayData(pokename) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokename}`;
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    for (let i = 0; i < currentPokemon['abilities'].length; i++) {
+        document.getElementById('detailData').innerHTML += `<p class="abilityParagraph">${currentPokemon['abilities'][i]['ability']['name']}</p>`;
+    }
+    if (currentPokemon['types'].length > 1) {
+        document.getElementById('detailTypes').innerHTML += `<h3>Typen:</h3>`
+    } else {
+        document.getElementById('detailTypes').innerHTML += `<h2>Typ:</h2>`
+    }
+    for (let i = 0; i < currentPokemon['types'].length; i++) {
+        document.getElementById('detailTypes').innerHTML += `<p class="abilityParagraph">${currentPokemon['types'][i]['type']['name']}</p>`;
+    }
+}
+
+/**
+ * function which is loaded first from details.html
+ */
+function initDetailBg() {
     randomizePixel();
     setInterval(movePixel, 20);
     loadPokedetails();
     setTitle();
 }
 
+/**
+ * function to set the title
+ * @param {String} titleName - name which will set to document title
+ */
 function setTitle(titleName) {
     document.title = titleName;
 }
 
+/**
+ * function to move the pixels in the background
+ * if the pixels are outside the screen, they are randomly placed on the screen
+ */
 function movePixel() {
     let pixel = document.getElementsByClassName('pixel');
     for (let i = 0; i < pixel.length; i++) {
         pixel[i].style.left = (pixel[i].offsetLeft + (Math.random() * 5) - 2) + 'px';
         pixel[i].style.top = (pixel[i].offsetTop + Math.random() * 10) - 4 + 'px';
-        let windowHeightReset = window.innerHeight - 170;
-        let windowWidthReset = window.innerWidth - 170;
+        let windowHeightReset = window.innerHeight * 0.8;
+        let windowWidthReset = window.innerWidth * 0.8;
         if (pixel[i].offsetTop > windowHeightReset || pixel[i].offsetTop < 0 || pixel[i].offsetLeft > windowWidthReset || pixel[i].offsetLeft < 0) {
             pixel[i].style.top = windowHeightReset * Math.random() + 'px';
             pixel[i].style.left = (Math.random() * windowWidthReset) + 'px';
@@ -218,15 +285,13 @@ function movePixel() {
     }
 }
 
+/**
+ * 
+ */
 function randomizePixel() {
     let pixel = document.getElementsByClassName('pixel');
     for (let i = 0; i < pixel.length; i++) {
         pixel[i].style.top = (window.innerHeight * 0.8) * Math.random() + 'px';
         pixel[i].style.left = (window.innerWidth * 0.8) * Math.random() + 'px';
     }
-}
-
-function openDetailSite(inputPoke) {
-    saveArrayToLocalStorage(localPoke, inputPoke);
-    window.location.href = 'details.html';
 }
